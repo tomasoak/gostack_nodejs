fa61c31955044d2932abcc384740b762859b8e15
Tomás Carvalho <tomas.jpeg@gmail.com>
2020-04-22 19:56:26 -0300

    add create setting

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,21 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
-  }
+  // public getBalance({ title, value, type }: CreateTransactionDTO): Balance {
+  // const balance = new Balance({ title, value, type });
 
-  public create(): Transaction {
-    // TODO
+  // const income = this.balance.find(title)
+  // }
+
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
